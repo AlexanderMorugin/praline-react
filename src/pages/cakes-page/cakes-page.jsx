e88e423a89c3b602a2b86fc752616f278afd8500
Alexander Morugin <nobilis@bk.ru>
2023-08-20 19:00:05 +0300

    start-2

--- src/pages/cakes-page/cakes-page.jsx
+++ src/pages/cakes-page/cakes-page.jsx
@@ -1,12 +1,35 @@
 import styles from "./cakes-page.module.css";
+import napoleon from "../../images/napoleon_tort-xxl-2.jpg";
 
 export const CakesPage = () => {
   return (
     <main className={styles.main}>
       <h1>Cakes Page</h1>
-      <div className={styles.card}>
-        <div className={styles.price}>200</div>
-
+      <div className={styles.cards}>
+        <div className={styles.card}>
+          <img className={styles.image} src={napoleon} alt="napoleon" />
+          <p className={styles.image_title}>Торт Наполеон</p>
+          <p className={styles.image_subtitle}>Торт приготовленный из хрустящего слоеного теста и мягкого сливочного крема</p>
+          <div className={styles.price}>200 руб</div>
+        </div>
+        <div className={styles.card}>
+          <img className={styles.image} src={napoleon} alt="napoleon" />
+          <p className={styles.image_title}>Торт Наполеон</p>
+          <p className={styles.image_subtitle}>Торт приготовленный из хрустящего слоеного теста и мягкого сливочного крема</p>
+          <div className={styles.price}>200 руб</div>
+        </div>
+        <div className={styles.card}>
+          <img className={styles.image} src={napoleon} alt="napoleon" />
+          <p className={styles.image_title}>Торт Наполеон</p>
+          <p className={styles.image_subtitle}>Торт приготовленный из хрустящего слоеного теста и мягкого сливочного крема</p>
+          <div className={styles.price}>200 руб</div>
+        </div>
+        <div className={styles.card}>
+          <img className={styles.image} src={napoleon} alt="napoleon" />
+          <p className={styles.image_title}>Торт Наполеон</p>
+          <p className={styles.image_subtitle}>Торт приготовленный из хрустящего слоеного теста и мягкого сливочного крема</p>
+          <div className={styles.price}>200 руб</div>
+        </div>
       </div>
     </main>
   );
