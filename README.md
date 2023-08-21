61144d455d4f24d7e8bb83060d5d7c77076b91ad
Alexander Morugin <nobilis@bk.ru>
2023-08-21 13:54:25 +0300

    start-3

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# [praline-react](https://alexandermorugin.github.io/praline-react/)
+
+
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/pages/cakes-page/cakes-page.jsx
+++ src/pages/cakes-page/cakes-page.jsx
@@ -4,30 +4,31 @@ import napoleon from "../../images/napoleon_tort-xxl-2.jpg";
 export const CakesPage = () => {
   return (
     <main className={styles.main}>
-      <h1>Cakes Page</h1>
+      <h1 className={styles.title}>Торты</h1>
+      <p className={styles.description}>Цены на торты для кафе, ресторанов, пекарен, баров, на заказ с доставкой, от кондитерской Пралине</p>
       <div className={styles.cards}>
         <div className={styles.card}>
           <img className={styles.image} src={napoleon} alt="napoleon" />
           <p className={styles.image_title}>Торт Наполеон</p>
-          <p className={styles.image_subtitle}>Торт приготовленный из хрустящего слоеного теста и мягкого сливочного крема</p>
+          <p className={styles.image_description}>Торт приготовленный из хрустящего слоеного теста и мягкого сливочного крема</p>
           <div className={styles.price}>200 руб</div>
         </div>
         <div className={styles.card}>
           <img className={styles.image} src={napoleon} alt="napoleon" />
           <p className={styles.image_title}>Торт Наполеон</p>
-          <p className={styles.image_subtitle}>Торт приготовленный из хрустящего слоеного теста и мягкого сливочного крема</p>
+          <p className={styles.image_description}>Торт приготовленный из хрустящего слоеного теста и мягкого сливочного крема</p>
           <div className={styles.price}>200 руб</div>
         </div>
         <div className={styles.card}>
           <img className={styles.image} src={napoleon} alt="napoleon" />
           <p className={styles.image_title}>Торт Наполеон</p>
-          <p className={styles.image_subtitle}>Торт приготовленный из хрустящего слоеного теста и мягкого сливочного крема</p>
+          <p className={styles.image_description}>Торт приготовленный из хрустящего слоеного теста и мягкого сливочного крема</p>
           <div className={styles.price}>200 руб</div>
         </div>
         <div className={styles.card}>
           <img className={styles.image} src={napoleon} alt="napoleon" />
           <p className={styles.image_title}>Торт Наполеон</p>
-          <p className={styles.image_subtitle}>Торт приготовленный из хрустящего слоеного теста и мягкого сливочного крема</p>
+          <p className={styles.image_description}>Торт приготовленный из хрустящего слоеного теста и мягкого сливочного крема</p>
           <div className={styles.price}>200 руб</div>
         </div>
       </div>
